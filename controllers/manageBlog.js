a6b8c786f649e9ffb596c6531441eb2c30859465
rajat@Axovel <rajat@arahantechsol.com>
2017-12-26 15:15:39 +0530

    code api

--- controllers/manageBlog.js
+++ controllers/manageBlog.js
@@ -6,7 +6,12 @@ var Blog = require('../models/blog');
 module.exports = {
   createBlogCategory: createBlogCategory,
   getAllBlogCategory: getAllBlogCategory,
-  getAllBlogCategoryById:getAllBlogCategoryById
+  getAllBlogCategoryById:getAllBlogCategoryById,
+  createBlog: createBlog,
+  getAllBlogs:getAllBlogs,
+  getBlogById: getBlogById,
+  getBlogBysubcategory: getBlogBysubcategory,
+  getRecentBlog:getRecentBlog,
 }
 
   function createBlogCategory(req, res, next) {
@@ -72,3 +77,96 @@ module.exports = {
       }
     });
   }
+
+  function createBlog(req, res, next){
+    // blog = Blog.Blog;
+    blog = Blog.Blog;
+    blog.findOne({_id: req.body._id}).then(blogDoc => {
+      if(blogDoc) {
+        blog.findOneAndUpdate({_id: req.body._id}, req.body, {new: true}).then(doc => {
+          return res.status(200)
+            .json(doc);
+        },err => {
+          return next(err);
+        });
+      }else {
+        newBlog = new Blog.Blog(req.body);
+        newBlog.blogCreationDate = new Date();
+        newBlog.save().then(newBlog => {
+          return res.status(200)
+            .json(newBlog);
+        },err => {
+          return next(err);
+        });
+      }
+    });
+  }
+
+  function getAllBlogs(req, res, next) {
+    blog = Blog.Blog;
+    blog.find().exec(function(err, blogDoc) {
+      if(err) {
+        return next(err);
+      }
+      if(blogDoc) {
+        return res.status(200)
+          .json(blogDoc);
+      }else {
+        return res.status(404)
+          .json({message: "Sorry there is no Blog"});
+      }
+    })
+  }
+
+  function getBlogById(req, res, next) {
+    blog = Blog.Blog;
+    console.log(req.params);
+
+    blog.findOne({_id: req.params.blogId}).then(blogDoc => {
+      if(blogDoc) {
+        return res.status(200)
+          .json(blogDoc);
+      } else {
+        return res.status(404)
+          .json({message: "Sorry there is no Blog"});
+      }
+    })
+  }
+
+
+    function getBlogBysubcategory(req, res, next) {
+      blog = Blog.Blog;
+      console.log(req.params);
+      console.log(req.params.subCategoryKey);
+      blog.find({ category: req.params.category,subCategory: req.params.subCategory}).then(blogDoc => {
+        if(blogDoc) {
+          return res.status(200)
+            .json(blogDoc);
+        } else {
+          return res.status(404)
+            .json({message: "Sorry there is no Blog"});
+        }
+      })
+    }
+
+  function getRecentBlog(req, res, next) {
+    blog = Blog.Blog;
+    blog.find().sort({blogCreationDate: 'desc'}).exec(function(err, blogDoc) {
+      if(err) {
+        return next(err);
+      }
+      if(blogDoc) {
+        return res.status(200)
+          .json(blogDoc);
+      }else {
+        return res.status(404)
+          .json({message: "Sorry there is no Blog"});
+      }
+    })
+  }
+
+
+  // function getBlogBysubcategory(req, res, next) {
+  //   blog = Blog.Blog;
+  //   console.log(req.params);
+  // }
--- routes/api.js
+++ routes/api.js
@@ -7,6 +7,12 @@ var manageBlog = require('../controllers/manageBlog');
 router.post('/subscribeMail', subscribeMail.subscribeMail);
 router.post('/createCategory', manageBlog.createBlogCategory);
 router.get('/getAllCategory', manageBlog.getAllBlogCategory);
-router.get('/getAllBlogCategoryById', manageBlog.getAllBlogCategoryById)
+router.get('/getAllBlogCategoryById', manageBlog.getAllBlogCategoryById);
+router.post('/createBlog', manageBlog.createBlog);
+router.get('/getAllBlogs', manageBlog.getAllBlogs);
+router.get('/getBlogById/:blogId', manageBlog.getBlogById);
+router.get('/getBlogBysubcategory/:category/:subCategory', manageBlog.getBlogBysubcategory);
+router.get('/getRecentBlog', manageBlog.getRecentBlog);
+// router.get('/getBlogBysubcategory', manageBlog.getBlogBysubcategory);
 
 module.exports = router;
